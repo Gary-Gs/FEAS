3eb82744a5db542080124e22dfea8cbb1963598c
xmkan <xuemin_gal@hotmail.com>
2019-05-09 15:15:54 +0800

    Update submit_saveas.php

--- fyp/fulltime/alloc/submit_saveas.php
+++ fyp/fulltime/alloc/submit_saveas.php
@@ -16,12 +16,12 @@ $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 if($_SERVER['HTTP_REFERER'] != null){
 	$urlString = explode('/', $_SERVER['HTTP_REFERER']);
 	$foldername = $urlString[0];
-	if(strcmp($foldername. 'https:') == 0){
+	if(strcmp($foldername, 'https:') == 0){
 		if(strcmp($_SERVER['HTTP_REFERER'], 'https://155.69.100.32/fyp/fulltime/alloc/allocation_setting.php') != 0){
 			throw new Exception($_SERVER['Invalid referer']);
 		}
 	}
-	elseif(strcmp($foldername. 'http:') == 0){
+	elseif(strcmp($foldername,'http:') == 0){
 		if(strcmp($_SERVER['HTTP_REFERER'], 'http://155.69.100.32/fyp/fulltime/alloc/allocation_setting.php') != 0){
 			throw new Exception($_SERVER['Invalid referer']);
 		}
