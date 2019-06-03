cdc0e5153a865776f2b102681d4c7745aa44549f
ZiHui <oon_zi_hui@hotmail.com>
2019-06-04 00:39:51 +0800

    change setcookie

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ session_start();
 
 //remove PHPSESSID from browser
 if (isset($_COOKIE[session_name()]))
-	setcookie(session_name(), “”, time()-7200);
+	setcookie( session_name(), “”, time()-3600, “/”);
 
 //clear session from globals
 $_SESSION = array();
