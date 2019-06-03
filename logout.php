04c19074908002396061f00d1f89b4649e73b1d5
ZiHui <oon_zi_hui@hotmail.com>
2019-06-03 22:47:13 +0800

    security

--- logout.php
+++ logout.php
@@ -1,12 +1,21 @@
-<?php 
+<?php
 session_start();
-session_destroy();
 
+//clear session from globals
+$_SESSION = array();
+
+//remove PHPSESSID from browser
+if (isset($_COOKIE[session_name()]))
+	setcookie(session_name(), “”, time()-7200);
+
+//clear session from disk
+session_destroy();
 ?>
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<?php require_once('head.php'); ?>	
+	<?php require_once('head.php'); ?>
 	<title>Logout</title>
 
 </head>
@@ -15,17 +24,17 @@ session_destroy();
 <?php require_once('php_css/headerwologin.php');?>
 	<div style="min-height: 87vh;">
 		<div class="container-fluid text-center" style="background: white; opacity: 0.8; filter: alpha(opacity=80); margin-top: -8px;">
-			
+
 				 <?php if(isset($_GET["session_expired"])) {
 				           echo "<h3>Session has timed out! Please login again! </h3>";
 			           }else {?>
-					   
+
 					<h3> You have logged out successfully.</h3>
 					<?php }?>
 			         <h3>Click <a href="login.php">here</a> to return to the login page</h3>
-		   
+
 		 </div>
 	 </div>
-	 <?php require_once('footer.php'); ?>	
+	 <?php require_once('footer.php'); ?>
 </body>
 </html>
