baac6bbcc267c2f8f1ea9962f8270251f6915996
ZiHui <oon_zi_hui@hotmail.com>
2019-06-28 04:41:23 +0800

    minor fix

--- fyp/fulltime/alloc/examiner_setting.php
+++ fyp/fulltime/alloc/examiner_setting.php
@@ -94,7 +94,7 @@ $conn_db_ntu = null;
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Faculty Settings</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
     <script type="text/javascript">
         function checkExamine(val) {
